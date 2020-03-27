c08bffaced590a983af1465f68057e5d97f57565
stacru <hamaw8888@gmail.com>
2020-03-28 01:41:11 +0900

    cookieモジュールで名前登録機能

--- app.js
+++ app.js
@@ -7,27 +7,30 @@ const qs = require('querystring');
 
 const index_ejs = fs.readFileSync('./index.ejs', 'utf8');
 
-let userName;
-
 const server = http.createServer((req, res)=>{
-  let content = ejs.render(index_ejs, {});
 
-  let body = '';
-  
   if(req.method == 'POST'){
+    let body = '';
     req.on('data', (data)=>{
       body += data
     });
     req.on('end', ()=>{
-      userName = qs.parse(body).userName;
-      console.log(`userName: ${userName} をcookieに保存しました`);
-      res.setHeader('Set-Cookie', [`userName=${escape(userName)}`])
+      let userName = qs.parse(body).userName;
+      console.log('userName@POST: ' + userName);
+      const setCookie = Cookie.serialize('userName', userName);
+      res.setHeader('Set-Cookie', [setCookie])
+      console.log('setCookie: ' + setCookie);
+
+      let content = ejs.render(index_ejs, {'userName': userName});
       res.writeHead(200, {'Content-Type':'text/html'});
       res.write(content);
       res.end();
     });
   } else {
-    res.setHeader('Set-Cookie', [`userName=${escape(userName)}`])
+    let userName = Cookie.parse(req.headers.cookie).userName;
+    console.log('userName@GET: ' + userName);
+
+    let content = ejs.render(index_ejs, {'userName':userName});
     res.writeHead(200, {'Content-Type':'text/html'});
     res.write(content);
     res.end();
